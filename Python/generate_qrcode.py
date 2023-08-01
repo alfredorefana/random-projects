644f5e7fdf638e459dd3fe8efb10151339846c03
Refana Alfredo Ndremaninazafy <alfredo.refana@outlook.com>
2023-08-01 12:32:11 +0200

    added description of parameters and methods

--- Python/generate_qrcode.py
+++ Python/generate_qrcode.py
@@ -13,14 +13,16 @@ from PIL import Image
 # Insert here the data you want to encode in the QR Code 
 data = "https://jaolahy.com"
 
-# Generate QR-code
-# You can modify and adjust the size and frame of your QR-Code
-qr = qrcode.QRCode(version = 1, box_size = 10, border=5)
-qr.add_data(data)
-qr.make(fit = True)
+# Generate QR-code. You can modify and adjust the size and frame of your QR-Code
+# Version: This parameter is an integer from 1 to 40 that controls the size of the QR Code (the smallest, version 1, is a 21×21 matrix).
+# box_size: This parameter controls how many pixels each “box” of the QR code is.
+# border: The border parameter controls how many boxes thick the border should be (the default is 4, which is the minimum in the specification).
+qr = qrcode.QRCode(version = 1, box_size = 10, border = 5)
+qr.add_data(data)       # add_data(): This method is used to add data to the QRCode object. It takes the data to be encoded as a parameter.
+qr.make(fit = True)     # make(): This method with (fit=True) ensures that the entire dimension of the QR Code is utilized, even if our input data could fit into less number of boxes.
 
-# Create an image from the generated QR-Code
-# You can change the colors
+# Create an image from the generated QR-Code. You can change the colors
+# make_image(): This method is used to convert the QRCode object into an image file. It takes the fill_color and back_color optional parameters to set the foreground and background color.
 image = qr.make_image(fill_color = "black", back_color = "white")
 
 # Save the created image in the parent directory the script location
