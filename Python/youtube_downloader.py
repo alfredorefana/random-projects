fdeaeabade4f2c835c2ed09936becd5fd529aab0
Refana Alfredo Ndremaninazafy <alfredo.refana@outlook.com>
2023-11-19 20:47:25 +0100

    Added choice of what to donwload.

--- Python/youtube_downloader.py
+++ Python/youtube_downloader.py
@@ -39,14 +39,35 @@ def mp3Downloader(youtubeObject, video_title):
         print("Oops ... Something is wrong...")
     print("\n Download is completed successfully.")
 
+def tasks(choix):
+    """ Function for the dowload. """
+    if choix == 1:
+        # For downloading MP3
+        mp3Downloader(youtubeObject, video_title)
+    elif choix == 2:
+        # For downloading video with the highest available resolution 
+        videoDowloader(youtubeObject, video_title)
+    elif choix == 3:
+        mp3Downloader(youtubeObject, video_title)
+        videoDowloader(youtubeObject, video_title)
+
 if __name__ == '__main__':
     link = input("Copy here the YouTube video URL: \n>> ")
     youtubeObject = YouTube(link, 
                         on_progress_callback=on_progress)
     video_title = youtubeObject.title
     
-    # For downloading MP3
-    mp3Downloader(youtubeObject, video_title)
+    choix = int(input("What do you want to donwload? \n 1. Audio\n 2. Video\n 3. Both\n >>  "))
 
-    # For downloading video with the highest available resolution 
-    videoDowloader(youtubeObject, video_title)
+    if choix == 1:
+        # For downloading MP3
+        mp3Downloader(youtubeObject, video_title)
+    elif choix == 2:
+        # For downloading video with the highest available resolution 
+        videoDowloader(youtubeObject, video_title)
+    elif choix == 3:
+        mp3Downloader(youtubeObject, video_title)
+        videoDowloader(youtubeObject, video_title)
+    else:
+        print("Goddamn it. Choose from the number above: 1 or 2 or 3.")
+        choix = int(input("What do you want to donwload? \n 1. Audio\n 2. Video\n 3. Both\n >>  "))
