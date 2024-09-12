4a38b2ed25819155beec92d500df9bcb32fa4bc9
Refana Alfredo Ndremaninazafy <32684053+alfredorefana@users.noreply.github.com>
2024-09-12 11:05:33 +0200

    Updated the formatting.

--- Bash/backup_for_docker_apps.sh
+++ Bash/backup_for_docker_apps.sh
@@ -10,14 +10,14 @@ BACKUP_CMD='/bin/tar -cvzf'
 cd $WORK_DIR
 
 for app in *; do
-        echo '\n ============================================== \n'
+        echo "============= Backing up $app ==============="
 
         DEST_BKP_FILE="$DEST_DIR/$app-$TIME_STAMP.tar.gz"
         sudo $BACKUP_CMD $DEST_BKP_FILE $app/*
         echo 'The generated backup file is: ' $DEST_BKP_FILE
 
-        echo '\n ============================================== \n'
+        echo "=============================================="
 done
 
-# Deleting old backups for more than 5 days
+# Deleting old backups
 /usr/bin/find $DEST_DIR -mtime +5 -delete
